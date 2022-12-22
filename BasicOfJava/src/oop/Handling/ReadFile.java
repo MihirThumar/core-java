1d07d2159cb43de7bfbbd2d6ad0f0a210b0eb979
MihirThumar <mihirthumaraaa@gmail.com>
2022-12-22 12:34:23 +0530

    Fifth

--- BasicOfJava/src/oop/Handling/ReadFile.java
+++ BasicOfJava/src/oop/Handling/ReadFile.java
@@ -1,7 +1,13 @@
 package oop.Handling;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 /* Practical 24
@@ -12,14 +18,67 @@ public class ReadFile {
 
 	public static void main(String[] args) {
 
-		try {
 		File file = new File("C:\\Users\\admin\\Desktop\\myTestFile.txt");
+//		Using Scanner
+		try {
 		Scanner scanner = new Scanner(file);
 			while(scanner.hasNextLine()) {
 				System.out.println(scanner.nextLine());
 			}
 			scanner.close();
 		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println();
+
+//		Uisng RandomAccessFile
+		try {
+			RandomAccessFile file2 = new RandomAccessFile("C:\\Users\\admin\\Desktop\\myTestFile.txt", "r");
+			String str;
+			while((str=file2.readLine())!=null) {
+				System.out.println(str);
+			}
+			file2.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println();
+
+//		Using BufferReader
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String st;
+			while((st = br.readLine())!=null) {
+				System.out.println(st);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println();
+
+//		Using FileReader
+		try {
+			FileReader fr = new FileReader(file);
+			int i;
+			while((i = fr.read()) != -1) {
+				System.out.print((char)i);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println();
+		System.out.println();
+
+//		Using String
+		try {
+			String data;
+			data = new String(Files.readAllBytes(Paths.get("C:\\Users\\admin\\Desktop\\myTestFile.txt")));
+			System.out.println(data);
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
